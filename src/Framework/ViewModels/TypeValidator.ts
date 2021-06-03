7d9ee6f2c512474ffc79565ba81e575f960e4843
Kitamura Satohi <satooshi.jp@gmail.com>
2021-06-04 07:41:02 +0900

    Prefer functions over static methods

--- src/Framework/ViewModels/TypeValidator.ts
+++ src/Framework/ViewModels/TypeValidator.ts
@@ -1,13 +1,11 @@
-export class TypeValidator {
-  static isInteger(value: string) {
-    return /^(\d+)$/.test(value);
-  }
+export function isInteger(value: string) {
+  return /^(\d+)$/.test(value);
+}
 
-  static isSignedInteger(value: string) {
-    return /^[-+]?(\d+)$/.test(value);
-  }
+export function isSignedInteger(value: string) {
+  return /^[-+]?(\d+)$/.test(value);
+}
 
-  static isFloat(value: string) {
-    return !Number.isNaN(Number(value)) && !Number.isNaN(parseFloat(value));
-  }
+export function isFloat(value: string) {
+  return !Number.isNaN(Number(value)) && !Number.isNaN(parseFloat(value));
 }
