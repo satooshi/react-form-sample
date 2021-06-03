1fe6f604dcf1974b4a5ace4be8cc74f6cb82a65a
Kitamura Satohi <satooshi.jp@gmail.com>
2021-06-04 07:38:50 +0900

    Prefer named exports

--- src/Apps/SimpleForm/UseCases/FooRepositoryInterface.d.ts
+++ src/Apps/SimpleForm/UseCases/FooRepositoryInterface.d.ts
@@ -1,5 +1,5 @@
 import { Repository } from "Framework/UseCases/Interfaces";
-import FormViewModel from '../ViewModels/FormViewModel';
+import { FormViewModel } from '../ViewModels/FormViewModel';
 
 export interface FooRepositoryInterface extends Repository<FormViewModel> {
   create: (viewModel: FormViewModel) => Promise<any>;
