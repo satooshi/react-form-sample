105a1d0c17ac56b0b498113b14daadf1dee04321
Kitamura Satohi <satooshi.jp@gmail.com>
2021-06-04 08:13:28 +0900

    Introduce Constructable

--- src/Framework/Bridges/ReactBridge.ts
+++ src/Framework/Bridges/ReactBridge.ts
@@ -1,23 +1,28 @@
 import { useState, useRef, useEffect } from 'react';
 import { ViewModel } from '../ViewModels/Interfaces';
 
+interface Constructable<T> {
+  new (...args: any): T; // eslint-disable-line @typescript-eslint/no-explicit-any
+}
+
 // const [viewModel, setViewModel] = useViewModel(new FormViewModel(initState));
 export function useViewModel<T extends ViewModel>(initialViewModel: T) {
   const [state, setState] = useState(initialViewModel.serialized);
-  const ctor = initialViewModel.constructor;
-  const currentViewModel: T = new (<any>ctor)(state); // eslint-disable-line @typescript-eslint/no-explicit-any
+  const ctor = initialViewModel.constructor as Constructable<T>;
+  const currentViewModel: T = new ctor(state); // eslint-disable-line new-cap
 
   const setViewModel = (nextViewModel: T) => {
     setState(nextViewModel.serialized);
   };
+
   return [currentViewModel, setViewModel] as [T, typeof setViewModel];
 }
 
 // Not needed
 export function useViewModelRef<T extends ViewModel>(initialViewModel: T) {
   const [state, setState] = useState(initialViewModel.serialized);
-  const ctor = initialViewModel.constructor;
-  const currentViewModel: T = new (<any>ctor)(state); // eslint-disable-line @typescript-eslint/no-explicit-any
+  const ctor = initialViewModel.constructor as Constructable<T>;
+  const currentViewModel: T = new ctor(state); // eslint-disable-line new-cap
 
   const refViewModel = useRef(currentViewModel);
   useEffect(() => {
