47dcdd7043dbe4f1f711e0c35b1667dffcceabb0
Kitamura Satohi <satooshi.jp@gmail.com>
2021-06-05 15:39:04 +0900

    Add PasswordInput

---
+++ src/Framework/Views/Components/Bootstrap/PasswordInput.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { debuglog } from 'Utils';
+import { ErrorMessage } from './Atoms/ErrorMessage';
+import { FormLabel } from './Atoms/FormLabel';
+
+type Props = {
+  error?: string;
+  id: string;
+  labelText: string;
+  onChange: (value: string) => void;
+  value?: string;
+};
+
+export const PasswordInput: React.VFC<Props> = React.memo(
+  ({ error, id, labelText, onChange, value }) => {
+    debuglog('render TextInput', { id, value });
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+      onChange(event.target.value);
+    }
+
+    return (
+      <>
+        <FormLabel id={id} labelText={labelText} />
+        <input
+          type="password"
+          id={id}
+          className={error ? 'form-control is-invalid' : 'form-control'}
+          onChange={handleChange}
+          value={value}
+        />
+        <ErrorMessage error={error} />
+      </>
+    );
+  }
+);
