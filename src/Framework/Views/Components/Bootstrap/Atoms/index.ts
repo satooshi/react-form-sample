e5420e092fbfe929fdeab552ab6028be8bf1b180
Kitamura Satohi <satooshi.jp@gmail.com>
2021-06-20 15:01:26 +0900

    Add index to atoms

---
+++ src/Framework/Views/Components/Bootstrap/Atoms/index.ts
@@ -0,0 +1,4 @@
+export { ErrorMessage } from './ErrorMessage';
+export { FormCheckInput } from './FormCheckInput';
+export { FormCheckLabel } from './FormCheckLabel';
+export { FormLabel } from './FormLabel';
